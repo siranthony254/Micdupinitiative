f38abce1a0b53d0fcc51988c49dd7d2f6f985364
siranthony254 <officialsiranthony@gmail.com>
2026-02-03 13:17:37 +0300

    modified:   next.config.ts 	modified:   package-lock.json 	modified:   package.json

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
+const repoName = "mui-web";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+
+  basePath: `/${repoName}`,
+  assetPrefix: `/${repoName}/`,
+
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
