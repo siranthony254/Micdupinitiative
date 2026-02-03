2a8435c84880bd0ea485c12ca88e88ca84733133
siranthony254 <officialsiranthony@gmail.com>
2026-02-03 22:19:53 +0300

    modified:   tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,8 @@ const repoName = "mui-web";
 module.exports = {
   content: [
     "./src/app/**/*.{ts,tsx}",
-    "./src/components/**/*.{ts,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
