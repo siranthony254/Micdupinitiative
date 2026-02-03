5d632cf4a615d996ce2fe8f446a39dc8e139b6f7
siranthony254 <officialsiranthony@gmail.com>
2026-02-03 13:54:59 +0300

    modified:   next.config.ts 	modified:   src/app/globals.css

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,4 @@
+// next.config.ts
 import type { NextConfig } from "next";
 
 const repoName = "mui-web";
@@ -5,13 +6,10 @@ const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig: NextConfig = {
   output: "export",
-  distDir: "out", 
-
   ...(isProd && {
     basePath: `/${repoName}`,
     assetPrefix: `/${repoName}/`,
   }),
-
   images: {
     unoptimized: true,
   },
