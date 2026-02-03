0a9b1a002d3ff540ab9e5b5c03b7fe5c914e6dd9
siranthony254 <officialsiranthony@gmail.com>
2026-02-03 22:11:47 +0300

    modified:   package-lock.json 	modified:   package.json 	renamed:    postcss.config.js -> postcss.config.mjs 	modified:   src/app/globals.css 	modified:   tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,10 @@ const repoName = "mui-web";
 module.exports = {
   content: [
     "./src/app/**/*.{ts,tsx}",
-    "./src/components/**/*.{ts,tsx}"
+    "./src/components/**/*.{ts,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
