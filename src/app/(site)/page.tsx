fb0510e81f39c7f8dca0ea6cec6fcbbb0c1d534a
siranthony254 <officialsiranthony@gmail.com>
2026-02-05 15:37:30 +0300

    modified:   src/app/(site)/page.tsx

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -2,7 +2,6 @@ import { HomePage as HeroSection }  from "@/components/home/hero";
 import  WhyWeExist  from "@/components/home/WhyWeExist";
 import { WhatWeDo } from "@/components/home/WhatWeDo";
 import { CategoryRail } from "@/components/discover/CategoryRail";
-import { DiscoverGrid } from "@/components/discover/DiscoverGrid";
 import { DiscoverHero } from "@/components/discover/DiscoverHero";
 import { PlaylistsRail } from "@/components/home/PlaylistsRail"; 
 import  GetInvolved  from "@/components/home/GetInvolved";
@@ -46,7 +45,6 @@ export default function HomePage() {
       <WhyWeExist />
       <WhatWeDo />
       <CategoryRail />
-      <DiscoverGrid /> 
       <DiscoverHero />
       <PlaylistsRail title="Featured Playlists" playlists={featuredPlaylists} />
       <GetInvolved />
