78119bbf5020b093ccf7c905996c88f53afcc22b
siranthony254 <officialsiranthony@gmail.com>
2026-05-10 21:12:59 +0300

    modified:   next.config.js

--- next.config.js
+++ next.config.js
@@ -42,15 +42,6 @@ const nextConfig = {
   // Performance headers for static assets
   headers: async () => [
     {
-      source: '/_next/static/(.*)',
-      headers: [
-        {
-          key: 'Cache-Control',
-          value: 'public, max-age=31536000, immutable',
-        },
-      ],
-    },
-    {
       source: '/images/(.*)',
       headers: [
         {
